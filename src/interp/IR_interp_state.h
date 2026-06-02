d339c97be4a6973fda53205b495c888e2e621595
LCherryholmes <lcherryh@yahoo.com>
2026-06-02 23:19:40 +0000

    LI-2 (slice B): sno_prog_t/sno_stmt_t -> prog_t/stmt_t, IR_SNO_PROG -> IR_PROG (shared IR/interp types, language-independent)

--- src/interp/IR_interp_state.h
+++ src/interp/IR_interp_state.h
@@ -18,8 +18,8 @@ typedef struct {
 } bb_node_state_t;
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------------------------------------------------*/
-typedef struct { IR_t * root; int succ_idx; int fail_idx; int is_terminal; } sno_stmt_t;
-typedef struct { sno_stmt_t * stmts; int n; int entry_idx; } sno_prog_t;
+typedef struct { IR_t * root; int succ_idx; int fail_idx; int is_terminal; } stmt_t;
+typedef struct { stmt_t * stmts; int n; int entry_idx; } prog_t;
 typedef struct { IR_t ** kids; int nkids; IR_graph_t * inner; int * pos_stack; int cap; int saved_delta; } bb_arbno_state_t;
 typedef struct { IR_t ** goals; int ngoals; } bb_conj_state_t;
 typedef struct { IR_t * cond; IR_t * then_; IR_t * else_; IR_t * then_root; IR_t * else_root; IR_t * cond_root; } bb_ite_state_t;
