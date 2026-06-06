51575a1fbce6971f53a977b4b7a0c63775965a66
LCherryholmes <lcherryh@yahoo.com>
2026-06-06 23:12:56 +0000

    SNO-HY-6: bb_pat_len + bb_pat_pos regenerated SPEC-v2 (real Greek, no locals, one return)

--- src/emitter/BB_templates/bb_pat_len.cpp
+++ src/emitter/BB_templates/bb_pat_len.cpp
@@ -9,20 +9,17 @@ extern "C" {
 static inline long lenN() { return (long)(int)_.op_ival; }
 /*--------------------------------------------------------------------------------------------------------------------*/
 static std::string bb_pat_len_str() {
-    int nid = _.nid; int sid = 0;
     if (PLATFORM_X86)
-        return IF(MEDIUM_TEXT,
-                   x86("label", _.lbl_α)
-                 + x86("comment", emit_fmt("BOX LEN(%ld)  [REG-2 δ=r14 Δ=r15, x86() self-encoding]", lenN())))
+        return IF(MEDIUM_TEXT, x86("label", _.lbl_α)
+                             + x86("comment", emit_fmt("BOX LEN(%ld)  [Σ=r13 δ=r14 Δ=r15, x86() self-encoding]", lenN())))
              + x86("mov", "eax", "r14d")
              + x86("add", "eax", lenN())
              + x86("cmp", "eax", "r15d")
-             + x86("jg",  PORT_OMEGA)
+             + x86("jg",  "\xCF\x89")
              + x86("add", "r14d", lenN())
-             + x86("jmp", PORT_GAMMA)
-             + x86("def", PORT_BETA)
-             + x86("jmp", PORT_OMEGA);
-/*--------------------------------------------------------------------------------------------------------------------*/
+             + x86("jmp", "\xCE\xB3")
+             + x86("def", "\xCE\xB2")
+             + x86("jmp", "\xCF\x89");
     return std::string();
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
