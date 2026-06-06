24bdc82167ae881084354c258ea5d9d7120fed72
LCherryholmes <lcherryh@yahoo.com>
2026-06-06 23:12:56 +0000

    SNO-HY-6: bb_pat_len + bb_pat_pos regenerated SPEC-v2 (real Greek, no locals, one return)

--- src/emitter/BB_templates/bb_pat_pos.cpp
+++ src/emitter/BB_templates/bb_pat_pos.cpp
@@ -10,22 +10,19 @@ static inline long posN()    { return (long)(int)_.op_ival; }
 static inline int  is_rpos() { return _.op_sval && _.op_sval[0] == 'r'; }
 /*--------------------------------------------------------------------------------------------------------------------*/
 static std::string bb_pat_pos_str() {
-    int nid = _.nid; (void)nid;
-    if (PLATFORM_X86) {
-        return IF(MEDIUM_TEXT,
-                   x86("label", _.lbl_α)
-                 + x86("comment", is_rpos() ? "# BOX RPOS()  [REG-3 δ=r14 Δ=r15, x86() self-encoding]"
-                                       : "# BOX POS()  [REG-3 δ=r14, x86() self-encoding]"))
+    if (PLATFORM_X86)
+        return IF(MEDIUM_TEXT, x86("label", _.lbl_α)
+                             + x86("comment", is_rpos() ? "BOX RPOS()  [Σ=r13 δ=r14 Δ=r15, x86() self-encoding]"
+                                                        : "BOX POS()  [Σ=r13 δ=r14 Δ=r15, x86() self-encoding]"))
              + (is_rpos()
                   ? ( x86("mov", "ecx", "r15d")
                     + x86("sub", "ecx", posN())
                     + x86("cmp", "r14d", "ecx") )
                   : ( x86("cmp", "r14d", posN()) ))
-             + x86("jne", PORT_OMEGA)
-             + x86("jmp", PORT_GAMMA)
-             + x86("def", PORT_BETA)
-             + x86("jmp", PORT_OMEGA);
-    }
+             + x86("jne", "\xCF\x89")
+             + x86("jmp", "\xCE\xB3")
+             + x86("def", "\xCE\xB2")
+             + x86("jmp", "\xCF\x89");
     return std::string();
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
