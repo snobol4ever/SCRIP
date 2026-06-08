b2877ca3a6e256c2f925870f38eb7e91dcd6abc9
LCherryholmes <lcherryh@yahoo.com>
2026-06-08 23:43:38 +0000

    FIXUP bb_det_write (v3): one-return lazy-IF + R1 terse IR_DET_WRITE + R2 inline+delete bdwr_tail + R4 drop MEDIUM_TEXT head-wrappers + ro_load_q/ro_seal_str via x86() dispatch (12->0 CLEAN); emitted-asm byte-identical (sole delta = R1 comment text), m2=m3=m4 hello/42/foo; cursor UNMOVED (out-of-cursor, bb_aggregate_nb hot per law-4 e6b4fd6 t->op rename <6h)

--- src/emitter/BB_templates/bb_det_write.cpp
+++ src/emitter/BB_templates/bb_det_write.cpp
@@ -9,25 +9,24 @@ extern "C" void rt_write_atom(const char * s);
 extern "C" void rt_write_int(long v);
 extern "C" void rt_pl_write_cell(void * cell_term);
 /*--------------------------------------------------------------------------------------------------------------------*/
-static std::string bdwr_tail() { return x86("jmp", "γ") + x86("def", "β") + x86("jmp", "ω"); }
 static std::string bb_det_write_str() {
     if (!PLATFORM_X86) return std::string();
     x86_begin();
-    if (_.op_sb)
-        return IF(MEDIUM_TEXT, x86("label", _.lbl_α) + x86("comment", "BOX DET_WRITE(cell)  [PL-GZ-3: Term* from [ζ+off], deref+print via ONE rt VALUE call]"))
-             + x86("mov", "rdi", FRQ(GZ_CELL_OFF(_.op_off)))
-             + x86("call", "rt_pl_write_cell", (uint64_t)(uintptr_t)(void *)rt_pl_write_cell)
-             + bdwr_tail();
-    if (_.op_sval)
-        return IF(MEDIUM_TEXT, x86("label", _.lbl_α) + x86("comment", "BOX DET_WRITE(atom)  [PL-GZ det VALUE call; operand sealed RO in-box, [rip+disp]]"))
-             + x86_ro_load_q("rdi", 0)
-             + x86("call", "rt_write_atom", (uint64_t)(uintptr_t)(void *)rt_write_atom)
-             + bdwr_tail()
-             + x86_ro_seal_str(0, _.op_sval);
-    return IF(MEDIUM_TEXT, x86("label", _.lbl_α) + x86("comment", "BOX DET_WRITE(int)  [PL-GZ det VALUE call]"))
-         + x86("mov", "rdi", (long)_.op_ival)
-         + x86("call", "rt_write_int", (uint64_t)(uintptr_t)(void *)rt_write_int)
-         + bdwr_tail();
+    return x86("label", _.lbl_α)
+         + x86("comment", "IR_DET_WRITE")
+         + IF(_.op_sb,
+               x86("mov", "rdi", FRQ(GZ_CELL_OFF(_.op_off)))
+             + x86("call", "rt_pl_write_cell", (uint64_t)(uintptr_t)(void *)rt_pl_write_cell))
+         + IF(!_.op_sb && _.op_sval,
+               x86("ro_load_q", "rdi", 0)
+             + x86("call", "rt_write_atom", (uint64_t)(uintptr_t)(void *)rt_write_atom))
+         + IF(!_.op_sb && !_.op_sval,
+               x86("mov", "rdi", (long)_.op_ival)
+             + x86("call", "rt_write_int", (uint64_t)(uintptr_t)(void *)rt_write_int))
+         + x86("jmp", "γ")
+         + x86("def", "β")
+         + x86("jmp", "ω")
+         + IF(!_.op_sb && _.op_sval, x86("ro_seal_str", 0, _.op_sval));
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
 extern "C" void bb_det_write(void) { bb_emit_x86(bb_det_write_str()); }
