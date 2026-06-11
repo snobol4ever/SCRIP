131527fd51dc355b3372f673b4a7e157447ea73d
LCherryholmes <lcherryh@yahoo.com>
2026-06-11 01:02:50 +0000

    D7-RB-2 step-1/5: bb_pattern_cat ins*→rt_pattern_stitch_cat + IR_PATTERN_CAT m2 arm

--- src/emitter/BB_templates/bb_pattern_cat.cpp
+++ src/emitter/BB_templates/bb_pattern_cat.cpp
@@ -4,37 +4,33 @@ extern "C" {
 #include "bb_template_common.h"
 #include "emit_bb.h"
 #include "emit.h"
+#include "dtp.h"
 }
 #include "x86_asm.h"
 /*--------------------------------------------------------------------------------------------------------------------*/
-static inline std::string  pc_sa()    { return std::to_string((long)_.op_sa); }
-static inline std::string  pc_sb()    { return std::to_string((long)_.op_sb); }
-static inline std::string  pc_off()   { return std::to_string((long)_.op_off); }
+static inline std::string  pc_off()  { return std::to_string((long)_.op_off); }
+static inline std::string  pc_sa()   { return std::to_string((long)_.op_sa); }
+static inline std::string  pc_sb()   { return std::to_string((long)_.op_sb); }
+static inline uint64_t     pc_fn()   { void (*fp)(DTP_FRAG_t*,const DTP_FRAG_t*,const DTP_FRAG_t*) = rt_pattern_stitch_cat;
+                                       return (uint64_t)(uintptr_t)(void *)fp; }
 /*--------------------------------------------------------------------------------------------------------------------*/
 static std::string bb_pattern_cat_str() {
     if (PLATFORM_X86)
-        return IF(MEDIUM_TEXT,
-                   x86("label", _.lbl_α)
-                 + x86("comment", std::string("BOX PATTERN_CAT  [STITCH ζ=r12 frag@") + pc_off() + " <- @" + pc_sa() + " . @" + pc_sb() + "]"))
-             + x86("ins2", "mov", "rcx, [r12 + " + pc_sb() + "]")
-             + x86("ins2", "mov", "rdx, [r12 + " + pc_sa() + " + 8]")
-             + x86("ins2", "mov", "[rdx], rcx")
-             + x86("ins2", "mov", "rcx, [r12 + " + pc_sa() + " + 16]")
-             + x86("ins2", "lea", "rcx, [rcx + 8]")
-             + x86("ins2", "mov", "rdx, [r12 + " + pc_sb() + " + 16]")
-             + x86("ins2", "mov", "[rdx], rcx")
-             + x86("ins2", "mov", "rcx, [r12 + " + pc_sa() + "]")
-             + x86("ins2", "mov", "[r12 + " + pc_off() + "], rcx")
-             + x86("ins2", "mov", "rcx, [r12 + " + pc_sb() + " + 8]")
-             + x86("ins2", "mov", "[r12 + " + pc_off() + " + 8], rcx")
-             + x86("ins2", "mov", "rcx, [r12 + " + pc_sa() + " + 16]")
-             + x86("ins2", "mov", "[r12 + " + pc_off() + " + 16], rcx")
-             + x86("jmp", "γ")
-             + x86("def", "β")
-             + x86("jmp", "ω");
+        return x86("label", _.lbl_α)
+             + x86("comment", std::string("BOX PATTERN_CAT  [STITCH ζ=r12 frag@") + pc_off() + " <- @" + pc_sa() + " . @" + pc_sb() + "]")
+             + x86("lea",  "rdi", FRQ(_.op_off))
+             + x86("lea",  "rsi", FRQ(_.op_sa))
+             + x86("lea",  "rdx", FRQ(_.op_sb))
+             + x86("push", "rbx")
+             + x86("mov",  "rbx", "rsp")
+             + x86("and",  "rsp", -16L)
+             + x86("call", "rt_pattern_stitch_cat", pc_fn())
+             + x86("mov",  "rsp", "rbx")
+             + x86("pop",  "rbx")
+             + x86("jmp",  "γ")
+             + x86("def",  "β")
+             + x86("jmp",  "ω");
     return std::string();
 }
 /*--------------------------------------------------------------------------------------------------------------------*/
-extern "C" void bb_pattern_cat(void) {
-    bb_emit_x86(bb_pattern_cat_str());
-}
+extern "C" void bb_pattern_cat(void) { bb_emit_x86(bb_pattern_cat_str()); }
