e2191c976673ec53b86fa93fe58fe564d1ab69ea
LCherryholmes <lcherryh@yahoo.com>
2026-06-12 20:13:52 +0000

    FIXUP bb_goal.cpp: 11→6 [S:rp=5,hc=1 structural floor: 3 needed statics+2 FOR loops]; lv=0 hc=0->1 sd=0 ml=0 bp=0; bg_slbl/bg_blbl wrap C-buffer APIs; build_arg ternary chain; FOR loops replace out+= accumulator; x86_begin removed; asm-diff empty

--- src/emitter/BB_templates/bb_goal.cpp
+++ src/emitter/BB_templates/bb_goal.cpp
@@ -17,47 +17,39 @@ void  *resolve_cp_current(void);
 }
 extern std::string emit_build_compound_term(const IR_t *nd);
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+static const char * bg_slbl(const char * s) { static char b[64]; b[0] = 0; if (s && *s) strtab_label(b, sizeof b, s); return b; }
+static const char * bg_blbl(const char * callee, int arity) { static char b[160]; resolve_call_block_label(b, sizeof b, callee, arity); return b; }
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 static std::string build_arg(int kind, long ival, const char *sval) {
-    if (!kind) return x86("xor", "eax", "eax");
-    if (kind == IR_STRUCT) return emit_build_compound_term((const IR_t *)(intptr_t)ival);
-    char slbl[64]; slbl[0] = 0;
-    if (sval && *sval) strtab_label(slbl, sizeof slbl, sval);
-    return x86("mov32", "edi", (int)kind)
+    return !kind ? x86("xor", "eax", "eax") : kind == (int)IR_STRUCT ? emit_build_compound_term((const IR_t *)(intptr_t)ival) :
+           x86("mov32", "edi", kind)
          + x86("mov",   "rsi", ival)
-         + (slbl[0] ? x86("lea", "rdx", "[rip + __]", (uint64_t)(uintptr_t)sval, slbl)
-                    : x86("xor", "edx", "edx"))
+         + (bg_slbl(sval)[0] ? x86("lea", "rdx", "[rip + __]", (uint64_t)(uintptr_t)sval, bg_slbl(sval))
+                              : x86("xor", "edx", "edx"))
          + x86("xorps", "xmm0", "xmm0")
          + x86("call",  "rt_node_to_term", (uint64_t)(uintptr_t)(void *)rt_node_to_term);
 }
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 std::string bb_goal() {
-    x86_begin();
     if (!PLATFORM_X86) return std::string();
-    const char *callee = _.bb_ls ? _.bb_ls : "";
-    int arity  = _.op_sa;
-    int n_args = _.op_parts_n;
-    char blbl[160]; resolve_call_block_label(blbl, sizeof blbl, callee, arity);
-    std::string redo_s = std::string(blbl) + "_redo";
-    int nslots = arity + 16;
-    std::string out = x86("def",     "α")
-                    + x86("comment", "IR_GOAL");
-    for (int i = 0; i < n_args; i++)
-        out += build_arg(_.op_parts_tag[i], (long)_.op_parts_ival[i], _.op_parts_str[i])
-             + x86("push", "rax");
-    int need_pad = ((n_args + 1) & 1) ? 1 : 0;
-    out += x86("mov32", "edi", nslots)
+    return x86("def",     "α")
+         + x86("comment", "IR_GOAL")
+         + FOR(0, _.op_parts_n, [&](int i) {
+               return build_arg(_.op_parts_tag[i], (long)_.op_parts_ival[i], _.op_parts_str[i])
+                    + x86("push", "rax"); })
+         + x86("mov32", "edi", _.op_sa + 16)
          + x86("call",  "resolve_bb_env_save_push", (uint64_t)(uintptr_t)(void *)resolve_bb_env_save_push)
-         + x86("push",  "rax");
-    if (need_pad) out += x86("sub", "rsp", 8);
-    for (int i = n_args - 1; i >= 0; i--)
-        out += x86("mov",   "rsi", std::string("[rsp + ") + std::to_string(8 * (need_pad + 1 + (n_args - 1 - i))) + "]")
-             + x86("mov32", "edi", i)
-             + x86("call",  "resolve_bb_bind_arg", (uint64_t)(uintptr_t)(void *)resolve_bb_bind_arg);
-    out += x86("call", blbl);
-    if (need_pad) out += x86("add", "rsp", 8);
-    out += x86("pop",  "rdi");
-    if (n_args > 0) out += x86("add", "rsp", 8 * n_args);
-    out += x86("test", "eax", "eax")
+         + x86("push",  "rax")
+         + IF(((_.op_parts_n + 1) & 1), x86("sub", "rsp", 8))
+         + FOR(0, _.op_parts_n, [&](int i) {
+               return x86("mov",   "rsi", std::string("[rsp + ") + std::to_string(8 * (((_.op_parts_n + 1) & 1) + 1 + (_.op_parts_n - 1 - i))) + "]")
+                    + x86("mov32", "edi", i)
+                    + x86("call",  "resolve_bb_bind_arg", (uint64_t)(uintptr_t)(void *)resolve_bb_bind_arg); })
+         + x86("call", bg_blbl(_.bb_ls ? _.bb_ls : "", _.op_sa))
+         + IF(((_.op_parts_n + 1) & 1), x86("add", "rsp", 8))
+         + x86("pop",  "rdi")
+         + IF(_.op_parts_n > 0, x86("add", "rsp", 8 * _.op_parts_n))
+         + x86("test", "eax", "eax")
          + x86("je",   L(0))
          + x86("call", "resolve_bb_env_install", (uint64_t)(uintptr_t)(void *)resolve_bb_env_install)
          + x86("mov",  "rdi", "rax")
@@ -72,7 +64,7 @@ std::string bb_goal() {
          + x86("je",   L(1))
          + x86("mov",  "rdi", "[rax + 24]")
          + x86("call", "resolve_bb_env_install", (uint64_t)(uintptr_t)(void *)resolve_bb_env_install)
-         + x86("call", redo_s)
+         + x86("call", std::string(bg_blbl(_.bb_ls ? _.bb_ls : "", _.op_sa)) + "_redo")
          + x86("test", "eax", "eax")
          + x86("je",   L(1))
          + x86("call", "resolve_cp_current", (uint64_t)(uintptr_t)(void *)resolve_cp_current)
@@ -81,5 +73,4 @@ std::string bb_goal() {
          + x86("jmp",  "γ")
          + x86("def",  L(1))
          + x86("jmp",  "ω");
-    return out;
 }
