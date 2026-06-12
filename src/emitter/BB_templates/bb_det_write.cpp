37369c7d7e40cac6653acd2e61e30d16b80f6e9b
LCherryholmes <lcherryh@yahoo.com>
2026-06-12 18:18:18 +0000

    Prolog BB: writeq/write_canonical GZ admission +3 m3 (A2 partial)

--- src/emitter/BB_templates/bb_det_write.cpp
+++ src/emitter/BB_templates/bb_det_write.cpp
@@ -8,13 +8,21 @@ extern "C" {
 extern "C" void rt_write_atom(const char * s);
 extern "C" void rt_write_int(long v);
 extern "C" void rt_pl_write_cell(void * cell_term);
+extern "C" void rt_pl_writeq_cell(void * cell_term);
+extern "C" void rt_pl_write_canonical_cell(void * cell_term);
 /*--------------------------------------------------------------------------------------------------------------------*/
 std::string bb_det_write() {
     if (!PLATFORM_X86) return std::string();
     x86_begin();
     return x86("label", _.lbl_α)
          + x86("comment", "IR_DET_WRITE")
-         + IF(_.op_sb,
+         + IF(_.op_sb && _.op_ival == 1,
+               x86("mov", "rdi", FRQ(GZ_CELL_OFF(_.op_off)))
+             + x86("call", "rt_pl_writeq_cell", (uint64_t)(uintptr_t)(void *)rt_pl_writeq_cell))
+         + IF(_.op_sb && _.op_ival == 2,
+               x86("mov", "rdi", FRQ(GZ_CELL_OFF(_.op_off)))
+             + x86("call", "rt_pl_write_canonical_cell", (uint64_t)(uintptr_t)(void *)rt_pl_write_canonical_cell))
+         + IF(_.op_sb && _.op_ival == 0,
                x86("mov", "rdi", FRQ(GZ_CELL_OFF(_.op_off)))
              + x86("call", "rt_pl_write_cell", (uint64_t)(uintptr_t)(void *)rt_pl_write_cell))
          + IF(!_.op_sb && _.op_sval,
