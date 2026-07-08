cd5034c9811665db2be1e0fb88b96199d20ed80d
LCherryholmes <lcherryh@yahoo.com>
2026-07-08 21:55:05 +0000

    PL-RES-2/3 + nested-generator handle-keyed resume: (,)/2 goal arm (flatten+thread_goals); arith value-tree $ax_ family (min/max/gcd/xor/shifts/bitand-or/rem + unary abs/sign/trunc/floor/ceil/round/sqrt/trig/log/exp/float-parts/msb + pi const) with recursive lower_arith_val; is/2 -> $is_v; $cmp_ operands routed through value-tree; $faN lambda-lift registered in proc_table; ROOT FIX: rt_proc_call_gen_h returns activation handle, rt_proc_resume_frame resumes by handle (was: global-LIFO-top resume skipped inner clause tails in nested generators - the [1,,] blank-template bug). Rung suite 32->43 all three modes, m3==m4; prolog 5/5/5, icon 12/12x2.

--- src/templates/bb_call_proc_staged.cpp
+++ src/templates/bb_call_proc_staged.cpp
@@ -6,7 +6,9 @@ extern "C" {
 #include "bb_templates.h"
 DESCR_t rt_call_proc_descr(const char *name, int nargs);
 DESCR_t rt_proc_call_gen(const char *name, int nargs);
+DESCR_t rt_proc_call_gen_h(const char *name, int nargs, void **hout);
 DESCR_t rt_proc_resume_gen(void);
+DESCR_t rt_proc_resume_frame(void *frame);
 int  rt_proc_is_generator(const char *name);
 void rt_arg_stage(int idx, DESCR_t v);
 int  rt_proc_is_registered(const char *name);
@@ -89,8 +91,8 @@ static std::string bcps_bin_gen_arm() {
     int off = bcps_result_slot(); if (off < 0) return x86_bomb("bb_call_proc_staged: no LOWER slot grant (TMP-ERADICATE)");
     IR_graph_t ** argblks = (IR_graph_t **)(intptr_t)_.op_counter;
     uint64_t stage_fp; { void (*fp)(int, DESCR_t) = rt_arg_stage; stage_fp = (uint64_t)(uintptr_t)(void*)fp; }
-    uint64_t callg_fp; { DESCR_t (*fp)(const char *, int) = rt_proc_call_gen; callg_fp = (uint64_t)(uintptr_t)(void*)fp; }
-    uint64_t resumeg_fp; { DESCR_t (*fp)(void) = rt_proc_resume_gen; resumeg_fp = (uint64_t)(uintptr_t)(void*)fp; }
+    uint64_t callg_fp; { DESCR_t (*fp)(const char *, int, void **) = rt_proc_call_gen_h; callg_fp = (uint64_t)(uintptr_t)(void*)fp; }
+    uint64_t resumef_fp; { DESCR_t (*fp)(void *) = rt_proc_resume_frame; resumef_fp = (uint64_t)(uintptr_t)(void*)fp; }
     return x86_alpha()
          + FOR(0, (int)_.op_ival, [&](int i) {
         int slot = bcps_arg_slot(_.node, argblks, i);
@@ -98,16 +100,16 @@ static std::string bcps_bin_gen_arm() {
     })
          + x86("mov", "rdi", (uint64_t)(uintptr_t)(_.op_sval ? _.op_sval : ""))
          + x86("mov32", "esi", (long)_.op_ival)
-         + x86("call", "rt_proc_call_gen", callg_fp)
+         + x86("lea", "rdx", FRQ(off + 8))
+         + x86("call", "rt_proc_call_gen_h", callg_fp)
          + x86_frame_store64(off, "rax")
-         + x86_frame_store64(off + 8, "rdx")
          + x86("cmp", "eax", (long)99)
          + x86_omega("je")
          + x86_gamma()
          + x86_beta()
-         + x86("call", "rt_proc_resume_gen", resumeg_fp)
+         + x86_frame_load64("rdi", off + 8)
+         + x86("call", "rt_proc_resume_frame", resumef_fp)
          + x86_frame_store64(off, "rax")
-         + x86_frame_store64(off + 8, "rdx")
          + x86("cmp", "eax", (long)99)
          + x86_omega("je")
          + x86_gamma();
@@ -127,16 +129,16 @@ static std::string bcps_txt_gen_arm() {
          })
          + x86("directive", (std::string(" lea rdi, [rip + .Lcall") + std::to_string(_.nid) + "_pname]").c_str())
          + x86("mov", "esi", std::to_string((int)_.op_ival))
-         + x86("call", "rt_proc_call_gen@PLT")
+         + x86("lea", "rdx", FRQ(off + 8))
+         + x86("call", "rt_proc_call_gen_h@PLT")
          + x86("mov", FRQ(off), "rax")
-         + x86("mov", FRQ(off + 8), "rdx")
          + x86("cmp", "eax", "99")
          + x86_omega("je")
          + x86_gamma()
          + x86("label", _.lbl_β)
-         + x86("call", "rt_proc_resume_gen@PLT")
+         + x86("mov", "rdi", FRQ(off + 8))
+         + x86("call", "rt_proc_resume_frame@PLT")
          + x86("mov", FRQ(off), "rax")
-         + x86("mov", FRQ(off + 8), "rdx")
          + x86("cmp", "eax", "99")
          + x86_omega("je")
          + x86_gamma();
