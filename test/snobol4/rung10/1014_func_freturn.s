9719ebc7a50e8d83d4b6b35c5a6ba7dcb6f706d2
LCherryholmes <lcherryh@yahoo.com>
2026-07-08 22:32:56 +0000

    feature x86 .s artifacts: s10-eval-code

--- test/snobol4/rung10/1014_func_freturn.s
+++ test/snobol4/rung10/1014_func_freturn.s
@@ -14,8 +14,8 @@ __gva: .space 16, 0
 main:
   push rbp
   mov rbp, rsp
-  call core_lib_init@PLT
-  call rt_proc_reset@PLT
+  push rdi
+  push rsi
   lea rdi, [rip + __gva_names]
   lea rsi, [rip + __gva]
   mov edx, 1
@@ -24,112 +24,141 @@ main:
   call rt_frame@PLT
   mov rdi, rax
   xor esi, esi
-  call flat_α
+  call main_α
   xor eax, eax
+  mov rsp, rbp
   pop rbp
   ret
-flat_α:
+main_α:
 #=======================================================================================================================
-    .global flat_α
-    .global flat_β
-    .global flat_γ
-    .global flat_ω
+    .global main_α
+    .global main_β
+    .global main_γ
+    .global main_ω
 push r12
   mov r12, rdi
-  lea r10, [rip + Δ]
-flat_α_body:
-snoch0_n0_α:
-# IR_LIT_S
-bb1_α:
- mov qword ptr [r12 + 16], 1
- mov rax, qword ptr [rip + .Lx2_0]
- mov qword ptr [r12 + 24], rax
- jmp xgvarg1_done
- xgvarg1_β:
- jmp snoch0_n1_α
-.Lx2_0:
- .quad .Lx2_0_s
-.Lx2_0_s:
- .string "always_fail()"
-xgvarg1_done:
-bb2_α:
-# BOX IR_CALL define(...) -> rt_call_arr by-name [four-port, FAIL->ω.node]
-# marshal arg0 = producer-box slot [r12+16] -> [r12+48]
- mov rax, qword ptr [r12 + 16]
- mov qword ptr [r12 + 48], rax
- mov rax, qword ptr [r12 + 24]
+ push rsi
+ push rbp
+ mov rbp, rsp
+ and rsp, -16
+ call rt_zls_mark@PLT
+ mov rsp, rbp
+ pop rbp
+ mov qword ptr [r12 + 144], rax
+ pop rsi
+main_α_body:
+# IR_LIT_STRING
+ xchain0_n0_α:
+ mov qword ptr [r12 + 48], 1
+ mov rax, qword ptr [rip + .Lx1_0]
  mov qword ptr [r12 + 56], rax
+ jmp xchain0_n1_α
+ xchain0_n0_β:
+ jmp xchain0_n2_α
+.Lx1_0:
+ .quad .Lx1_0_s
+.Lx1_0_s:
+ .string "always_fail()"
+ xchain0_n1_α:
+# BOX IR_CALL define(...) -> rt_call_arr by-name [four-port, FAIL->ω.node]
+# marshal arg0 = producer-box slot [r12+48] -> [r12+32]
+ mov rax, qword ptr [r12 + 48]
+ mov qword ptr [r12 + 32], rax
+ mov rax, qword ptr [r12 + 56]
+ mov qword ptr [r12 + 40], rax
   .section .rodata
-  .Lbynamefn4: .string "define"
+  .Lbynamefn3: .string "define"
   .section .text
   .intel_syntax noprefix
-   lea rdi, [rip + .Lbynamefn4]
- lea rsi, [r12 + 48]
+   lea rdi, [rip + .Lbynamefn3]
+ lea rsi, [r12 + 32]
  mov edx, 1
  call rt_call_arr@PLT
- mov qword ptr [r12 + 32], rax
- mov qword ptr [r12 + 40], rdx
+ mov qword ptr [r12 + 16], rax
+ mov qword ptr [r12 + 24], rdx
  cmp eax, 99
- je snoch0_n1_α
- jmp snoch0_n1_α
-snoch0_n0_β:
- jmp snoch0_n1_α
-snoch0_n1_α:
-bb3_α:
+ je xchain0_n2_α
+ jmp xchain0_n2_α
+xchain0_n1_β:
+ jmp xchain0_n2_α
+ xchain0_n2_α:
 # BOX IR_CALL always_fail(...) -> rt_call_arr by-name [four-port, FAIL->ω.node]
   .section .rodata
-  .Lbynamefn6: .string "always_fail"
+  .Lbynamefn5: .string "always_fail"
   .section .text
   .intel_syntax noprefix
-   lea rdi, [rip + .Lbynamefn6]
- lea rsi, [r12 + 64]
+   lea rdi, [rip + .Lbynamefn5]
+ lea rsi, [r12 + 80]
  mov edx, 0
  call rt_call_arr@PLT
  mov qword ptr [r12 + 64], rax
  mov qword ptr [r12 + 72], rdx
  cmp eax, 99
- je snoch0_n3_α
- jmp snoch0_n2_α
-snoch0_n1_β:
- jmp snoch0_n3_α
-snoch0_n2_α:
-# IR_LIT_scalar
-bb4_α:
- jmp snoch0_n4_α
- snoch0_n2_β:
- jmp flat_γ
-snoch0_n3_α:
-# IR_LIT_scalar
-bb5_α:
- jmp snoch0_n5_α
- snoch0_n3_β:
- jmp flat_γ
-snoch0_n4_α:
-bb6_α:
-# IR_ASSIGN_LIT_S
- lea rdi, [rip + .S0]
- lea rsi, [rip + .S1]
- call rt_gvar_assign_str@PLT
- jmp flat_γ
- snoch0_n4_β:
- jmp flat_γ
-snoch0_n5_α:
-bb7_α:
-# IR_ASSIGN_LIT_S
- lea rdi, [rip + .S0]
- lea rsi, [rip + .S2]
- call rt_gvar_assign_str@PLT
- jmp flat_γ
- snoch0_n5_β:
- jmp flat_γ
-flat_β:
-jmp flat_ω
-flat_γ:
+ je xchain0_n4_α
+ jmp xchain0_n3_α
+xchain0_n2_β:
+ jmp xchain0_n4_α
+# IR_LIT_STRING
+ xchain0_n3_α:
+ mov qword ptr [r12 + 96], 1
+ mov rax, qword ptr [rip + .Lx6_0]
+ mov qword ptr [r12 + 104], rax
+ jmp xchain0_n5_α
+ xchain0_n3_β:
+ jmp main_γ
+.Lx6_0:
+ .quad .Lx6_0_s
+.Lx6_0_s:
+ .string "FAIL 1014/001: freturn should cause statement failure"
+# IR_LIT_STRING
+ xchain0_n4_α:
+ mov qword ptr [r12 + 128], 1
+ mov rax, qword ptr [rip + .Lx7_0]
+ mov qword ptr [r12 + 136], rax
+ jmp xchain0_n6_α
+ xchain0_n4_β:
+ jmp main_γ
+.Lx7_0:
+ .quad .Lx7_0_s
+.Lx7_0_s:
+ .string "PASS 1014_func_freturn (1/1)"
+# IR_ASSIGN gva
+ xchain0_n5_α:
+ mov rax, qword ptr [r12 + 96]
+ mov rdx, qword ptr [r12 + 104]
+ mov qword ptr [rbx + 0], rax
+ mov qword ptr [rbx + 8], rdx
+ mov qword ptr [r12 + 80], rax
+ mov qword ptr [r12 + 88], rdx
+ jmp main_γ
+ xchain0_n5_β:
+ jmp main_γ
+# IR_ASSIGN gva
+ xchain0_n6_α:
+ mov rax, qword ptr [r12 + 128]
+ mov rdx, qword ptr [r12 + 136]
+ mov qword ptr [rbx + 0], rax
+ mov qword ptr [rbx + 8], rdx
+ mov qword ptr [r12 + 112], rax
+ mov qword ptr [r12 + 120], rdx
+ jmp main_γ
+ xchain0_n6_β:
+ jmp main_γ
+main_β:
+jmp main_ω
+main_γ:
 mov eax, 1
 xor edx, edx
 pop r12
 ret
-flat_ω:
+ push rbp
+ mov rbp, rsp
+ and rsp, -16
+ mov rdi, qword ptr [r12 + 144]
+ call rt_zls_release_to@PLT
+ mov rsp, rbp
+ pop rbp
+main_ω:
 # GZ-10 PROC FAIL EXIT: write FAILDESCR to frame[0] so rt_call_proc_descr sees failure
 mov dword ptr [r12+0], 99
 mov dword ptr [r12+4], 0
@@ -138,8 +167,3 @@ mov eax, 99
 xor edx, edx
 pop r12
 ret
-.section .rodata
-.S0: .string "output"
-.S1: .string "FAIL 1014/001: freturn should cause statement failure"
-.S2: .string "PASS 1014_func_freturn (1/1)"
-.text
