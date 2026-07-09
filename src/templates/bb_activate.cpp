28e7bdcc14bb3cc8a4c2dceb32d6440e7ae7c836
LCherryholmes <lcherryh@yahoo.com>
2026-07-09 22:09:17 +0000

    FIXUP bb_activate.cpp: 4->0 (lv1 s-local dissolved, rp2 lazy-IF two-return fold incl bombs, bp1 frame_lea->x86 lea FRQ); C2 normalized A/B EXACTLY EMPTY x2 coexpr probes (59/60, IR_ACTIVATE live), m3=m4 behavior parity, gates at floors

--- src/templates/bb_activate.cpp
+++ src/templates/bb_activate.cpp
@@ -11,23 +11,23 @@ extern "C" {
 std::string bb_activate() {
     x86_begin();
     if (!PLATFORM_X86) return std::string();
-    if (_.op_off < 0) return x86_alpha() + x86_bomb("bb_activate: op_off < 0 (no slot assigned -- IR_ACTIVATE missing from ir_node_produces_value?)");
-    if (_.op_sa  < 0) return x86_alpha() + x86_bomb("bb_activate: no coexpression operand slot (operand[0] unregistered -- LOWER/BFS wiring bug)");
-    std::string s = x86("comment", "IR_ACTIVATE")
-                  + x86_alpha()
-                  + x86("mov",     "rdi", FRQ(_.op_sa));
-    if (_.op_sb >= 0)
-        s += x86("mov", "rsi", FRQ(_.op_sb))
-           + x86("mov", "rdx", FRQ(_.op_sb + 8));
-    else
-        s += x86("xor", "esi", "esi")
-           + x86("xor", "edx", "edx");
-    s += x86_frame_lea("rcx", _.op_off)
-       + x86("call", "scrip_coexpr_activate", (uint64_t)(uintptr_t)(void *)scrip_coexpr_activate)
-       + x86("test", "rax", "rax")
-       + x86_omega("je")
-       + x86_gamma()
-       + x86_beta()
-       + x86_omega();
-    return s;
+    return IF(_.op_off < 0, x86_alpha() + x86_bomb("bb_activate: op_off < 0 (no slot assigned -- IR_ACTIVATE missing from ir_node_produces_value?)"))
+         + IF(_.op_off >= 0 && _.op_sa < 0, x86_alpha() + x86_bomb("bb_activate: no coexpression operand slot (operand[0] unregistered -- LOWER/BFS wiring bug)"))
+         + IF(_.op_off >= 0 && _.op_sa >= 0,
+               x86("comment", "IR_ACTIVATE")
+             + x86_alpha()
+             + x86("mov",  "rdi", FRQ(_.op_sa))
+             + IF(_.op_sb >= 0,
+                   x86("mov", "rsi", FRQ(_.op_sb))
+                 + x86("mov", "rdx", FRQ(_.op_sb + 8)))
+             + IF(_.op_sb < 0,
+                   x86("xor", "esi", "esi")
+                 + x86("xor", "edx", "edx"))
+             + x86("lea",  "rcx", FRQ(_.op_off))
+             + x86("call", "scrip_coexpr_activate", (uint64_t)(uintptr_t)(void *)scrip_coexpr_activate)
+             + x86("test", "rax", "rax")
+             + x86_omega("je")
+             + x86_gamma()
+             + x86_beta()
+             + x86_omega());
 }
