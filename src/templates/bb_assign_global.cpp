828a0c66d2cf658a8057afdf7c679f5fe5cf9735
LCherryholmes <lcherryh@yahoo.com>
2026-07-09 22:22:14 +0000

    FIXUP bb_assign_global.cpp: 2->0 (rp1 arms folded to complementary lazy-IF one-return, over_col bomb line reflowed); C2 A/B EXACTLY EMPTY x2 probes BOTH arms live (sno NV + icn gva), m3=m4 parity

--- src/templates/bb_assign_global.cpp
+++ src/templates/bb_assign_global.cpp
@@ -10,32 +10,35 @@ DESCR_t NV_SET_fn(const char * name, DESCR_t val);
 #include "x86_asm.h"
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 std::string bb_assign_global() {
-    if (!(PLATFORM_X86 && _.op_a_slot >= 0 && _.op_off >= 0)) return x86_alpha() + x86_bomb((std::string("bb_assign_global: unhandled (needs descr flat-chain + rhs slot + own slot) var=") + (_.op_sval ? _.op_sval : "?")).c_str());
-    if (g_gva_active && _.op_gva_k >= 0)
-        return x86("comment", "IR_ASSIGN gva")
-             + x86_alpha()
-             + x86("mov",    "rax", FRQ(_.op_a_slot))
-             + x86("mov",    "rdx", FRQ(_.op_a_slot + 8))
-             + x86("mov",    RDQ("rbx", _.op_gva_k * 16),     "rax")
-             + x86("mov",    RDQ("rbx", _.op_gva_k * 16 + 8), "rdx")
-             + x86("mov",    FRQ(_.op_off),     "rax")
-             + x86("mov",    FRQ(_.op_off + 8), "rdx")
-             + x86_gamma()
-             + x86_beta()
-             + x86_omega();
-    return x86("comment", "IR_ASSIGN global")
-         + x86_alpha()
-         + x86("mov",    "rsi", FRQ(_.op_a_slot))
-         + x86("mov",    "rdx", FRQ(_.op_a_slot + 8))
-         + x86("mov",    "rdi", ROQ(0))
-         + x86("call",   "NV_SET_fn", (uint64_t)(uintptr_t)(void *)(DESCR_t (*)(const char *, DESCR_t))NV_SET_fn)
-         + x86("mov",    FRQ(_.op_off),     "rax")
-         + x86("mov",    FRQ(_.op_off + 8), "rdx")
-         + x86_gamma()
-         + x86_beta()
-         + x86_omega()
-         + x86("def",    L(0))
-         + x86(".quad",  LS(0), _.op_sval)
-         + x86("label",  LS(0))
-         + x86(".string", _.op_sval);
+    if (!(PLATFORM_X86 && _.op_a_slot >= 0 && _.op_off >= 0))
+        return x86_alpha()
+             + x86_bomb((std::string("bb_assign_global: unhandled (needs descr flat-chain + rhs slot + own slot) var=") + (_.op_sval ? _.op_sval : "?")).c_str());
+    return IF(g_gva_active && _.op_gva_k >= 0,
+              x86("comment", "IR_ASSIGN gva")
+            + x86_alpha()
+            + x86("mov",    "rax", FRQ(_.op_a_slot))
+            + x86("mov",    "rdx", FRQ(_.op_a_slot + 8))
+            + x86("mov",    RDQ("rbx", _.op_gva_k * 16),     "rax")
+            + x86("mov",    RDQ("rbx", _.op_gva_k * 16 + 8), "rdx")
+            + x86("mov",    FRQ(_.op_off),     "rax")
+            + x86("mov",    FRQ(_.op_off + 8), "rdx")
+            + x86_gamma()
+            + x86_beta()
+            + x86_omega())
+         + IF(!(g_gva_active && _.op_gva_k >= 0),
+              x86("comment", "IR_ASSIGN global")
+            + x86_alpha()
+            + x86("mov",    "rsi", FRQ(_.op_a_slot))
+            + x86("mov",    "rdx", FRQ(_.op_a_slot + 8))
+            + x86("mov",    "rdi", ROQ(0))
+            + x86("call",   "NV_SET_fn", (uint64_t)(uintptr_t)(void *)(DESCR_t (*)(const char *, DESCR_t))NV_SET_fn)
+            + x86("mov",    FRQ(_.op_off),     "rax")
+            + x86("mov",    FRQ(_.op_off + 8), "rdx")
+            + x86_gamma()
+            + x86_beta()
+            + x86_omega()
+            + x86("def",    L(0))
+            + x86(".quad",  LS(0), _.op_sval)
+            + x86("label",  LS(0))
+            + x86(".string", _.op_sval));
 }
