2125cf041ac8acbd6d34b531c5c495c88b44973d
LCherryholmes <lcherryh@yahoo.com>
2026-07-17 19:43:01 +0000

    feature x86 .s artifacts: regen

--- test/snobol4/capture/059_capture_dollar_deferred.s
+++ test/snobol4/capture/059_capture_dollar_deferred.s
@@ -257,12 +257,12 @@ jmp main_ω
 main_γ:
 mov eax, 1
 xor edx, edx
-mov rsp, qword ptr [rsp + 408]
+mov rsp, qword ptr [rbp + 408]
 mov rbp, [rsp + 65536]
 add rsp, 65544
 ret
 main_ω:
-mov rsp, qword ptr [rsp + 408]
+mov rsp, qword ptr [rbp + 408]
 mov dword ptr [rsp+0], 99
 mov dword ptr [rsp+4], 0
 mov qword ptr [rsp+8], 0
--- test/snobol4/keywords/100_roman_numeral.s
+++ test/snobol4/keywords/100_roman_numeral.s
@@ -2599,12 +2599,12 @@ jmp main_ω
 main_γ:
 mov eax, 1
 xor edx, edx
-mov rsp, qword ptr [rsp + 5656]
+mov rsp, qword ptr [rbp + 5656]
 mov rbp, [rsp + 65536]
 add rsp, 65544
 ret
 main_ω:
-mov rsp, qword ptr [rsp + 5656]
+mov rsp, qword ptr [rbp + 5656]
 mov dword ptr [rsp+0], 99
 mov dword ptr [rsp+4], 0
 mov qword ptr [rsp+8], 0
