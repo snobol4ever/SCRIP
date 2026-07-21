80e65babf19ae4fd3e58816ff0a45ad0daeab567
LCherryholmes <lcherryh@yahoo.com>
2026-07-21 14:03:21 +0000

    BB-FIXUP A-to-Z: bb_enter_init.cpp 1->0 CLEAN (3->2 returns via IF(guard,bomb)+IF(!guard,main), keep !PLATFORM_X86 bomb; byte-identical, Icon smoke 14/14 m3+m4)

--- src/templates/bb_enter_init.cpp
+++ src/templates/bb_enter_init.cpp
@@ -10,13 +10,14 @@ extern "C" {
 std::string bb_enter_init() {
     x86_begin();
     if (!PLATFORM_X86) return x86_alpha() + x86_bomb("bb_enter_init: no x86 platform");
-    if (_.op_off < 0) return x86_alpha() + x86_bomb("bb_enter_init: op_off < 0 (no slot assigned)");
-    return x86_alpha()
-         + x86("comment", "IR_INITIAL")
-         + x86("mov",  "rax", FRQ(_.op_off + 8))
-         + x86("cmp",  "rax", (long)0)
-         + x86_omega("jne")
-         + x86("mov",  FRQ(_.op_off + 8), (long)1)
-         + x86_gamma()
-         + x86_beta_trampoline();
+    return IF(_.op_off < 0, x86_alpha() + x86_bomb("bb_enter_init: op_off < 0 (no slot assigned)"))
+         + IF(!(_.op_off < 0),
+             x86_alpha()
+           + x86("comment", "IR_INITIAL")
+           + x86("mov",  "rax", FRQ(_.op_off + 8))
+           + x86("cmp",  "rax", (long)0)
+           + x86_omega("jne")
+           + x86("mov",  FRQ(_.op_off + 8), (long)1)
+           + x86_gamma()
+           + x86_beta_trampoline());
 }
