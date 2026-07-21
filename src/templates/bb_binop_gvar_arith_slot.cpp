bbaa9cb7a3ac271a03aa4e59e64c122c7c50f5cc
LCherryholmes <lcherryh@yahoo.com>
2026-07-21 13:19:08 +0000

    BB-FIXUP A-to-Z: bb_binop_gvar_arith_slot.cpp 11->0 CLEAN (dormant box; ml-split 9, local m eliminated via IF-chains, 3->2 returns via ternary tail; A/B byte-identical 155 feature progs)

--- src/templates/bb_binop_gvar_arith_slot.cpp
+++ src/templates/bb_binop_gvar_arith_slot.cpp
@@ -14,34 +14,37 @@ DESCR_t NV_GET_fn(const char * name);
 #include "x86_asm.h"
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 std::string bb_binop_gvar_arith_slot() {
-    if (PLATFORM_X86 && _.op_arith_descr && _.op_sa >= 0 && _.op_sb >= 0 && _.op_off >= 0) {
-        std::string m;
-        if (_.bb_lk == (int)IR_VAR && _.op_name1 && _.op_parts_lbl[0])
-            m += x86("lea", "rdi", "[rip + __]", (uint64_t)(uintptr_t) _.op_name1, _.op_parts_lbl[0])
-               + x86("call", "NV_GET_fn", (uint64_t)(uintptr_t)(void *) NV_GET_fn)
-               + x86("mov", FRQ(_.op_sa), "rax") + x86("mov", FRQ(_.op_sa + 8), "rdx");
-        else if (_.bb_lk == (int)IR_LIT_INTEGER)
-            m += x86("movabs", "rax", (uint64_t)DT_I) + x86("mov", FRQ(_.op_sa), "rax")
-               + x86("movabs", "rax", (uint64_t)_.bb_li) + x86("mov", FRQ(_.op_sa + 8), "rax");
-        if (_.bb_rk == (int)IR_VAR && _.op_name2 && _.op_parts_lbl[1])
-            m += x86("lea", "rdi", "[rip + __]", (uint64_t)(uintptr_t) _.op_name2, _.op_parts_lbl[1])
-               + x86("call", "NV_GET_fn", (uint64_t)(uintptr_t)(void *) NV_GET_fn)
-               + x86("mov", FRQ(_.op_sb), "rax") + x86("mov", FRQ(_.op_sb + 8), "rdx");
-        else if (_.bb_rk == (int)IR_LIT_INTEGER)
-            m += x86("movabs", "rax", (uint64_t)DT_I) + x86("mov", FRQ(_.op_sb), "rax")
-               + x86("movabs", "rax", (uint64_t)_.bb_ri) + x86("mov", FRQ(_.op_sb + 8), "rax");
+    if (PLATFORM_X86 && _.op_arith_descr && _.op_sa >= 0 && _.op_sb >= 0 && _.op_off >= 0)
         return x86_alpha()
              + x86("comment", "IR_BINOP_GVAR_ARITH (dynamic operands: DESCR-in DESCR-out via rt_num_arith)")
-             + m
-             + x86("mov", "rdi", FRQ(_.op_sa)) + x86("mov", "rsi", FRQ(_.op_sa + 8))
-             + x86("mov", "rdx", FRQ(_.op_sb)) + x86("mov", "rcx", FRQ(_.op_sb + 8))
+             + IF(_.bb_lk == (int)IR_VAR && _.op_name1 && _.op_parts_lbl[0], x86("lea", "rdi", "[rip + __]", (uint64_t)(uintptr_t) _.op_name1, _.op_parts_lbl[0]))
+             + IF(_.bb_lk == (int)IR_VAR && _.op_name1 && _.op_parts_lbl[0], x86("call", "NV_GET_fn", (uint64_t)(uintptr_t)(void *) NV_GET_fn))
+             + IF(_.bb_lk == (int)IR_VAR && _.op_name1 && _.op_parts_lbl[0], x86("mov", FRQ(_.op_sa), "rax"))
+             + IF(_.bb_lk == (int)IR_VAR && _.op_name1 && _.op_parts_lbl[0], x86("mov", FRQ(_.op_sa + 8), "rdx"))
+             + IF(_.bb_lk == (int)IR_LIT_INTEGER, x86("movabs", "rax", (uint64_t)DT_I))
+             + IF(_.bb_lk == (int)IR_LIT_INTEGER, x86("mov", FRQ(_.op_sa), "rax"))
+             + IF(_.bb_lk == (int)IR_LIT_INTEGER, x86("movabs", "rax", (uint64_t)_.bb_li))
+             + IF(_.bb_lk == (int)IR_LIT_INTEGER, x86("mov", FRQ(_.op_sa + 8), "rax"))
+             + IF(_.bb_rk == (int)IR_VAR && _.op_name2 && _.op_parts_lbl[1], x86("lea", "rdi", "[rip + __]", (uint64_t)(uintptr_t) _.op_name2, _.op_parts_lbl[1]))
+             + IF(_.bb_rk == (int)IR_VAR && _.op_name2 && _.op_parts_lbl[1], x86("call", "NV_GET_fn", (uint64_t)(uintptr_t)(void *) NV_GET_fn))
+             + IF(_.bb_rk == (int)IR_VAR && _.op_name2 && _.op_parts_lbl[1], x86("mov", FRQ(_.op_sb), "rax"))
+             + IF(_.bb_rk == (int)IR_VAR && _.op_name2 && _.op_parts_lbl[1], x86("mov", FRQ(_.op_sb + 8), "rdx"))
+             + IF(_.bb_rk == (int)IR_LIT_INTEGER, x86("movabs", "rax", (uint64_t)DT_I))
+             + IF(_.bb_rk == (int)IR_LIT_INTEGER, x86("mov", FRQ(_.op_sb), "rax"))
+             + IF(_.bb_rk == (int)IR_LIT_INTEGER, x86("movabs", "rax", (uint64_t)_.bb_ri))
+             + IF(_.bb_rk == (int)IR_LIT_INTEGER, x86("mov", FRQ(_.op_sb + 8), "rax"))
+             + x86("mov", "rdi", FRQ(_.op_sa))
+             + x86("mov", "rsi", FRQ(_.op_sa + 8))
+             + x86("mov", "rdx", FRQ(_.op_sb))
+             + x86("mov", "rcx", FRQ(_.op_sb + 8))
              + x86("mov", "r8d", (long)_.op_ival)
              + x86("call", "rt_num_arith", (uint64_t)(uintptr_t)(void *) rt_num_arith)
-             + x86("cmp", "eax", (long)DT_FAIL) + x86_omega("je")
-             + x86("mov", FRQ(_.op_off), "rax") + x86("mov", FRQ(_.op_off + 8), "rdx")
+             + x86("cmp", "eax", (long)DT_FAIL)
+             + x86_omega("je")
+             + x86("mov", FRQ(_.op_off), "rax")
+             + x86("mov", FRQ(_.op_off + 8), "rdx")
              + x86_gamma() + x86_beta() + x86_omega();
-    }
-    if (PLATFORM_X86) return IF(_.op_off >= 0
+    return PLATFORM_X86 ? (IF(_.op_off >= 0
                               && (_.op_ival == BINOP_ADD || _.op_ival == BINOP_SUB || _.op_ival == BINOP_MUL || _.op_ival == BINOP_DIV || _.op_ival == BINOP_MOD)
                               && (_.bb_lk == (int)IR_LIT_INTEGER || (_.bb_lk == (int)IR_VAR && _.op_name1 != 0) || _.op_sa >= 0)
                               && (_.bb_rk == (int)IR_LIT_INTEGER || (_.bb_rk == (int)IR_VAR && _.op_name2 != 0) || _.op_sb >= 0),
@@ -75,6 +78,5 @@ std::string bb_binop_gvar_arith_slot() {
                               && (_.op_ival == BINOP_ADD || _.op_ival == BINOP_SUB || _.op_ival == BINOP_MUL || _.op_ival == BINOP_DIV || _.op_ival == BINOP_MOD)
                               && (_.bb_lk == (int)IR_LIT_INTEGER || (_.bb_lk == (int)IR_VAR && _.op_name1 != 0) || _.op_sa >= 0)
                               && (_.bb_rk == (int)IR_LIT_INTEGER || (_.bb_rk == (int)IR_VAR && _.op_name2 != 0) || _.op_sb >= 0)),
-                            x86_bomb("bb_binop_gvar_arith_slot: shape mismatch (dispatch chose this arm but predicate failed)"));
-    return std::string();
+                            x86_bomb("bb_binop_gvar_arith_slot: shape mismatch (dispatch chose this arm but predicate failed)"))) : std::string();
 }
