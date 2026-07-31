1261881638da1b862ef35cb2651d5d6b8e7daeb1
LCherryholmes <lcherryh@yahoo.com>
2026-07-31 17:12:09 +0000

    feature x86 .s artifacts: LP-2

--- test/snobol4/keywords/079_builtin_lt_le_ge.s
+++ test/snobol4/keywords/079_builtin_lt_le_ge.s
@@ -17,9 +17,9 @@ main_α:
                         .global          main_β
                         .global          main_γ
                         .global          main_ω
-                        sub              rsp, 8
+                        sub              rsp, 264
                         mov              rdi, rsp
-                        mov              ecx, 8
+                        mov              ecx, 264
                         xor              eax, eax
                         rep stosb
 main_α_body:
@@ -369,12 +369,12 @@ main_β:
 main_γ:
                         mov              eax, 1
                         xor              edx, edx
-                        add              rsp, 8
+                        add              rsp, 264
                         ret
 #-----------------------------------------------------------------------------------------------------------------------
 main_ω:
                         mov              eax, 99
                         xor              edx, edx
-                        add              rsp, 8
+                        add              rsp, 264
                         ret
                         .section         .note.GNU-stack,"",@progbits
