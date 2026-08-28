18e0588e01e777b488b1da26a72abab82816edcf
LCherryholmes <lcherryh@yahoo.com>
2026-08-28 08:18:10 -0500

    FIXUP bb_var_ref.cpp: R6 local inline (_fvr_fp indirection -> direct fn-ptr cast) + R4 split, 8->3 (rp floor: 3 legitimate early returns)

--- src/templates/bb/bb_var_ref.cpp
+++ src/templates/bb/bb_var_ref.cpp
@@ -14,10 +14,9 @@ std::string bb_var_ref() {
     x86_begin();
     if (_.op_off == -1) return x86_alpha() + x86_bomb("bb_var_ref: needs own slot");
     if (_.op_gva_k < 0 && _.op_sa == -1) {
-        uint64_t _fvr_fp; { DESCR_t (*_f)(void) = rt_pl_fresh_var_ref; _fvr_fp = (uint64_t)(uintptr_t)(void *)_f; }
         return x86("comment", "IR_VAR_REF anon: rt_pl_fresh_var_ref -> PLJ PLVAR cell")
              + x86_alpha()
-             + x86("call", "rt_pl_fresh_var_ref", (long)_fvr_fp)
+             + x86("call", "rt_pl_fresh_var_ref", (uint64_t)(uintptr_t)(void *)rt_pl_fresh_var_ref)
              + x86("mov", FRQ(_.op_off),     "rax")
              + x86("mov", FRQ(_.op_off + 8), "rdx")
              + x86_gamma()
@@ -27,18 +26,22 @@ std::string bb_var_ref() {
         return x86("comment", "IR_VAR_REF icn cells zd: NAMETRAP{DT_N,slen=1,&____slot} -> ZRES")
              + x86_alpha()
              + x86("mov", "rax", (long)((long)1 << 32 | (long)DT_N))
-             + x86("note", ZRESN()) + x86("mov", ZRES(0), "rax")
+             + x86("note", ZRESN())
+             + x86("mov", ZRES(0), "rax")
              + (_.op_gva_k >= 0
-                 ? x86("note", gva_name(_.op_gva_k)) + x86("mov", "rax", (long)(RT_GVA_VA + _.op_gva_k * 16))
+                 ? x86("note", gva_name(_.op_gva_k))
+                 + x86("mov", "rax", (long)(RT_GVA_VA + _.op_gva_k * 16))
                  : x86("lea", "rax", FRQ(_.op_sa)))
-             + x86("note", ZRESN()) + x86("mov", ZRES(8), "rax")
+             + x86("note", ZRESN())
+             + x86("mov", ZRES(8), "rax")
              + x86_gamma()
              + x86_beta_trampoline();
     return x86("comment", "IR_VAR_REF")
          + x86_alpha()
          + x86("mov", "rax", (long)((long)1 << 32 | (long)DT_N))
          + (_.op_gva_k >= 0
-             ? x86("note", gva_name(_.op_gva_k)) + x86("mov", "rdx", (long)(RT_GVA_VA + _.op_gva_k * 16))
+             ? x86("note", gva_name(_.op_gva_k))
+             + x86("mov", "rdx", (long)(RT_GVA_VA + _.op_gva_k * 16))
              : x86("lea", "rdx", FRQ(_.op_sa)))
          + x86("mov",     FRQ(_.op_off),     "rax")
          + x86("mov",     FRQ(_.op_off + 8), "rdx")
