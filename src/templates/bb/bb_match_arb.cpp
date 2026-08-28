12074d5b7f6927b4b0b56894ddd582295aae924f
LCherryholmes <lcherryh@yahoo.com>
2026-08-28 09:43:29 -0500

    FIXUP bb_match_arb.cpp: extracted ma_line1() helper (R6 + reused for diag), inlined _diag, wrapped >200-char diagnostic fprintf, 3->0 CLEAN

--- src/templates/bb/bb_match_arb.cpp
+++ src/templates/bb/bb_match_arb.cpp
@@ -6,14 +6,19 @@ extern "C" {
 }
 #include "x86_asm.h"
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-std::string bb_match_arb() {
-    x86_begin();
-    int _diag = getenv("SCRIP_ARB_DIAG") ? 1 : 0;
-    std::string _line1 = x86("comment", "IR_MATCH_ARB")
+static std::string ma_line1() {
+    return x86("comment", "IR_MATCH_ARB")
          + x86_alpha()
          + x86("mov",    LFC(0), (long)0);
-    if (_diag) fprintf(stderr, "[ARB] AFTER-LINE1 x86_scratch_off=%d op_fc_bytes=%ld op_fc_base=%ld op_zdepth=%d op_fb_data=%d zstorage=%d fc_on=%d fc_hit0=%d regime0=%d emitted_tail=%s\n", _.x86_scratch_off, (long)_.op_fc_bytes, (long)_.op_fc_base, _.op_zdepth, x86_fb_data(), (int)x86_zstorage(), x86_fc_on(), x86_fc_hit(_.x86_scratch_off), x86_zop_regime(_.x86_scratch_off), _line1.substr(_line1.size() > 80 ? _line1.size() - 80 : 0).c_str());
-    return _line1
+}
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+std::string bb_match_arb() {
+    x86_begin();
+    if (getenv("SCRIP_ARB_DIAG"))
+        fprintf(stderr, "[ARB] AFTER-LINE1 x86_scratch_off=%d op_fc_bytes=%ld op_fc_base=%ld op_zdepth=%d op_fb_data=%d zstorage=%d fc_on=%d fc_hit0=%d regime0=%d emitted_tail=%s\n",
+                _.x86_scratch_off, (long)_.op_fc_bytes, (long)_.op_fc_base, _.op_zdepth, x86_fb_data(), (int)x86_zstorage(), x86_fc_on(), x86_fc_hit(_.x86_scratch_off),
+                x86_zop_regime(_.x86_scratch_off), ma_line1().substr(ma_line1().size() > 80 ? ma_line1().size() - 80 : 0).c_str());
+    return ma_line1()
          + x86("mov",    "eax", "r14d")
          + x86("mov",    LFC(4), "eax")
          + x86_gamma()
