bc811f7c4bdf84d647d9ecbef76b281f81b8cd7b
LCherryholmes <lcherryh@yahoo.com>
2026-08-28 09:43:13 -0500

    FIXUP bb_match_rpos.cpp: R6 inline vn1 alias, dead redundant if/return collapsed, 3->1 (rp floor)

--- src/templates/bb/bb_match_rpos.cpp
+++ src/templates/bb/bb_match_rpos.cpp
@@ -23,10 +23,10 @@ static std::string bb_match_rpos_body() {
 std::string bb_match_rpos() {
     static char b[24];
     if (_.op_sval != NULL) {
-        const char * vn1 = _.op_sval + 1;
         return x86("comment", "IR_MATCH_RPOS defer")
              + x86_alpha()
-             + x86("lea",  "rdi", "[rip + __]", (uint64_t)(uintptr_t)(const void *)vn1, (strtab_label(b, sizeof b, vn1), b))
+             + x86("lea",  "rdi", "[rip + __]",
+                   (uint64_t)(uintptr_t)(const void *)(_.op_sval + 1), (strtab_label(b, sizeof b, _.op_sval + 1), b))
              + x86("call", "rt_pat_prim_int", (uint64_t)(uintptr_t)(void *)rt_pat_prim_int)
              + x86("test", "rax", "rax")
              + x86_omega("js")
@@ -37,6 +37,5 @@ std::string bb_match_rpos() {
              + x86_gamma()
              + x86_beta_trampoline();
     }
-    if (_.op_zres) return bb_match_rpos_body();
     return bb_match_rpos_body();
 }
