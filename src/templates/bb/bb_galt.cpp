c7c7b709ee21253b39dbd92520282d9b701df84a
LCherryholmes <lcherryh@yahoo.com>
2026-08-28 14:47:05 -0500

    FIXUP bb_galt.cpp: inline dptr (pure 2-use address-string local), 1->0 CLEAN

--- src/templates/bb/bb_galt.cpp
+++ src/templates/bb/bb_galt.cpp
@@ -10,13 +10,12 @@ std::string bb_galt() {
     x86_begin();
     if (_.op_off < 0 || !_.lbl_t0 || !_.lbl_t1 || !_.lbl_t0_p || !_.lbl_t1_p)
         return x86_alpha() + x86_bomb("bb_galt: IR_GALT missing dslot or arm labels");
-    std::string dptr = "dword ptr [rsp + " + std::to_string(_.op_off) + "]";
     return x86("comment", "IR_GALT (grammar alternation): save delta to [rsp+dslot] at alpha; restore+jmp-arm2 at beta")
          + x86_alpha()
-         + x86("mov", dptr, "r14d")
+         + x86("mov", "dword ptr [rsp + " + std::to_string(_.op_off) + "]", "r14d")
          + x86_jmp_lblptr(_.lbl_t0_p, _.lbl_t0)
          + x86_beta()
-         + x86("mov", "r14d", dptr)
+         + x86("mov", "r14d", "dword ptr [rsp + " + std::to_string(_.op_off) + "]")
          + x86_jmp_lblptr(_.lbl_t1_p, _.lbl_t1)
          + x86_gamma()
          + x86_omega();
