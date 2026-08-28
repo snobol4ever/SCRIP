abe52ad02a28d8862b4b43aed5177706bb74e1fe
LCherryholmes <lcherryh@yahoo.com>
2026-08-28 14:47:05 -0500

    FIXUP bb_limit.cpp: fold guard+emission into one complementary-IF() return, 1->0 CLEAN

--- src/templates/bb/bb_limit.cpp
+++ src/templates/bb/bb_limit.cpp
@@ -9,22 +9,22 @@ extern "C" {
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 std::string bb_limit() {
     x86_begin();
-    if (!(_.op_off >= 0 && _.op_sa >= 0 && _.op_sc >= 0 && _.lbl_t0))
-        return x86_alpha() + x86_bomb("bb_limit: unhandled (needs descr flat-chain, static slots, count slot, gen-β)");
-    return x86("comment", "IR_LIMIT")
-         + x86_alpha()
-         + x86("mov",   "rax", FRQ(_.op_off + 16))
-         + x86("mov",   "rcx", FRQ(_.op_sc + 8))
-         + x86("cmp",   "rax", "rcx")
-         + x86_omega("jge")
-         + x86("inc",   FRQ(_.op_off + 16))
-         + x86("mov",   "rax", FRQ(_.op_sa))
-         + x86("mov",   FRQ(_.op_off),     "rax")
-         + x86("mov",   "rax", FRQ(_.op_sa + 8))
-         + x86("mov",   FRQ(_.op_off + 8), "rax")
-         + x86_gamma()
-         + x86_beta()
-         + x86_jmp_tgt(X86T_TGT0);
+    return IF(!(_.op_off >= 0 && _.op_sa >= 0 && _.op_sc >= 0 && _.lbl_t0), x86_alpha() + x86_bomb("bb_limit: unhandled (needs descr flat-chain, static slots, count slot, gen-β)"))
+         + IF(_.op_off >= 0 && _.op_sa >= 0 && _.op_sc >= 0 && _.lbl_t0,
+             x86("comment", "IR_LIMIT")
+           + x86_alpha()
+           + x86("mov",   "rax", FRQ(_.op_off + 16))
+           + x86("mov",   "rcx", FRQ(_.op_sc + 8))
+           + x86("cmp",   "rax", "rcx")
+           + x86_omega("jge")
+           + x86("inc",   FRQ(_.op_off + 16))
+           + x86("mov",   "rax", FRQ(_.op_sa))
+           + x86("mov",   FRQ(_.op_off),     "rax")
+           + x86("mov",   "rax", FRQ(_.op_sa + 8))
+           + x86("mov",   FRQ(_.op_off + 8), "rax")
+           + x86_gamma()
+           + x86_beta()
+           + x86_jmp_tgt(X86T_TGT0));
 }
 /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 std::string bb_limit_init() {
